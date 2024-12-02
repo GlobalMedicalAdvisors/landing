7d2977b6bbacb673bf51e07ca4afa7072f12f231
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2024-12-02 22:53:49 +0200

    show a navlink on a layout level

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "@/components/navbar";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -27,6 +28,8 @@ export default function RootLayout({
       <body
         className={`${montserrat.className} antialiased container mx-auto flex flex-col min-h-screen`}
       >
+        <Navbar />
+
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,14 +12,12 @@ import {
   RefreshCw,
   Send,
 } from "lucide-react";
-import { Navbar } from "@/components/navbar";
 import { FeatureCard } from "@/components/feature-card";
 import { Badge } from "@/components/ui/badge";
 
 export default function LandingPage() {
   return (
     <div>
-      <Navbar />
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
           <div className="container px-4 md:px-6">
